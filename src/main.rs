1934a86f06926412962772d2fb10b9ae41595222
Ronald Lens <rflens@gmail.com>
2018-12-15 17:24:50 +0100

    render image into pixel buffer

--- src/main.rs
+++ src/main.rs
@@ -105,3 +105,28 @@ fn test_pixel_to_point() {
         Complex { re: -0.5, im: -0.5 }
     );
 }
+
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+///
+/// The `bounds` argument gives the width and height of the buffer `pixels`,
+/// which holds one gray scale pixel per byte. The `upper_left` and `lower_right`
+/// arguments specify the points on the complex plane corresponding to the upper-left
+/// and lower right corners of the pixel buffer.
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert_eq!(pixels.len(), bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bunds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
